b8b77be97fcef0dc50f3339ad6c21af52046c7cc
Ning Ma <ningma97@gmail.com>
2016-10-31 15:02:00 +0000

    updated topdown localiation example

--- localisation_DNNs_Topdown/localise.m
+++ localisation_DNNs_Topdown/localise.m
@@ -16,9 +16,9 @@ brirs = { ...
     'impulse_responses/qu_kemar_rooms/auditorium3/QU_KEMAR_Auditorium3_src5_xs-0.75_ys+1.30.sofa'; ...
     'impulse_responses/qu_kemar_rooms/auditorium3/QU_KEMAR_Auditorium3_src6_xs+0.75_ys+1.30.sofa'; ...
     };
-headOrientation = 90; % towards y-axis (facing src1)
+robotOrientation = 90; % towards y-axis (facing src1)
 sourceAzimuthsWorld = [90, 38.5, -41.4, 90, 120, 60];
-sourceAzimuths = sourceAzimuthsWorld - headOrientation;
+sourceAzimuths = sourceAzimuthsWorld - robotOrientation;
 nSourceAzimuths = length(sourceAzimuths);
 
 % === Setup binaural simulator
@@ -39,23 +39,23 @@ for ii = 1:nSourceAzimuths
     noiseAzimuth = sourceAzimuths(nn);
     sim.Sources{2}.IRDataset = simulator.DirectionalIR(brirs{nn});
 
-    % Rotate the head to the 0 degree
-    sim.rotateHead(headOrientation, 'absolute');
+    sim.moveRobot(0, 0, robotOrientation);
+    sim.rotateHead(0, 'absolute');
+    sim.Init = true;
+    phiNoTopdown = estimateAzimuth(sim, 'BlackboardDnn.xml');    % DnnLocationKS with top-down mask
     
-    % Initialise simulator
-    sim.set('Init', true);
-
-    phiTopdown = estimateAzimuth(sim, 'BlackboardDnnTopDown.xml');    % DnnLocationKS with top-down mask
-    resetBinauralSimulator(sim, headOrientation);
-    phiNoTopdown = estimateAzimuth(sim, 'BlackboardDnn.xml');           % DnnLocationKS without top-down mask
+    sim.moveRobot(0, 0, robotOrientation);
+    sim.rotateHead(0, 'absolute');
+    sim.ReInit = true;
+    phiTopdown = estimateAzimuth(sim, 'BlackboardDnnTopDown.xml');           % DnnLocationKS without top-down mask
 
     printLocalisationTableColumn(targetAzimuth, ...
                                  noiseAzimuth, ...
-                                 phiTopdown - headOrientation, ...
-                                 phiNoTopdown - headOrientation);
+                                 phiTopdown, ...
+                                 phiNoTopdown);
 
     % Clean up
-    sim.set('ShutDown',true);
+    sim.ShutDown = true;
 
 end
 
@@ -67,13 +67,13 @@ function printLocalisationTableHeader()
     fprintf('\n=== Two-source localisation\n');
     fprintf('----------------------------------------------------------------------------------------\n');
     fprintf('Ref. target azimuth    Ref. noise azimuth    Est. target azimuth    Est. target azimuth\n');
-    fprintf('                                             with topdown mask      without topdown mask\n');
+    fprintf('                                             without topdown mask      with topdown mask\n');
     fprintf('----------------------------------------------------------------------------------------\n');
 end
 
-function printLocalisationTableColumn(targetAz, noiseAz, phiTopdown, phiNoTopdown)
+function printLocalisationTableColumn(targetAz, noiseAz, phiNoTopdown, phiTopdown)
     fprintf('     %4.0f                  %4.0f                  %4.0f                   %4.0f\n', ...
-            wrapTo180(targetAz), wrapTo180(noiseAz), wrapTo180(phiTopdown), wrapTo180(phiNoTopdown));
+            wrapTo180(targetAz), wrapTo180(noiseAz), wrapTo180(phiNoTopdown), wrapTo180(phiTopdown));
 end
 
 function printLocalisationTableFooter()
